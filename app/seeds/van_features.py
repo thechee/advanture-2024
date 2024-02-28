fe89604b2be783b879bda36f8f8e23848c5adc40
Chase <chase.agee@gmail.com>
2024-02-28 15:24:46 -0800

    fixed seeds to work for join table

--- app/seeds/van_features.py
+++ app/seeds/van_features.py
@@ -1,22 +1,65 @@
-from app.models import db, van_features, environment, SCHEMA
+from app.models import db, Van, Feature, van_features, environment, SCHEMA
 from sqlalchemy.sql import text
 from .vans_seed_data import vans
+from .features_seed_data import features
 import random
 
 def seed_van_features():
+  # for van in vans:
+  #   for i in random.sample(range(13), 6):
+  #     db.session.add(van_features(
+  #         van_id=van['id'],
+  #         feature_id=i
+  #         ))
+  #     db.session.commit()
+    # for feature in features:
+    #     db.session.add(Feature(name=feature['name']))
+  newVans = []
   for van in vans:
-    for i in random.sample(range(13), 6):
-      db.session.add(van_features(
-          van_id=van['id'],
-          feature_id=i
-          ))
-      db.session.commit()
+    newVan = Van(
+    user_id=van['user_id'],
+    year=van['year'],
+    make=van['make'],
+    model=van['model'],
+    miles=van['miles'],
+    address=van['address'],
+    city=van['city'],
+    state=van['state'],
+    zip_code=van['zip_code'],
+    rental_rate=van['rental_rate'],
+    description=van['description'],
+    distance_allowed=van['distance_allowed'],
+    mpg=van['mpg'],
+    doors=van['doors'],
+    seats=van['seats'],
+    fuel_type_id=van['fuel_type_id']
+    )
 
+    newVans.append(newVan)
+
+  newFeatures = []
+  for feature in features:
+    newFeature = Feature(
+      name=feature['name']
+    )
+    newFeatures.append(newFeature)
+
+  for van in newVans:
+    for i in random.sample(range(13), 6):
+      van.features.append(newFeatures[i])
+  
+  db.session.add_all(newVans)
+  db.session.add_all(newFeatures)
+
+  db.session.commit()
 
 def undo_van_features():
     if environment == "production":
         db.session.execute(f"TRUNCATE table {SCHEMA}.van_features RESTART IDENTITY CASCADE;")
     else:
         db.session.execute(text("DELETE FROM van_features"))
+        db.session.execute(text("DELETE FROM vans"))
+        db.session.execute(text("DELETE FROM features"))
         
     db.session.commit()
+

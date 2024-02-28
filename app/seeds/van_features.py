bbdb9848b16325eae0c6019fd37f99363a96926c
Chase <chase.agee@gmail.com>
2024-02-28 13:55:50 -0800

    added van_features seeder file

--- app/seeds/van_features.py
+++ app/seeds/van_features.py
@@ -1,13 +1,19 @@
 from app.models import db, van_features, environment, SCHEMA
 from sqlalchemy.sql import text
 from .vans_seed_data import vans
+import random
 
 def seed_van_features():
   for van in vans:
-    
+    for i in random.sample(range(13), 6):
+      db.session.add(van_features(
+          van_id=van['id'],
+          feature_id=i
+          ))
+      db.session.commit()
 
 
-def undo_features():
+def undo_van_features():
     if environment == "production":
         db.session.execute(f"TRUNCATE table {SCHEMA}.van_features RESTART IDENTITY CASCADE;")
     else:

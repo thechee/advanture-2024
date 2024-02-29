f4e109e8b2f377f1d329d63d67d86c853fdb58bc
Chase <chase.agee@gmail.com>
2024-02-29 10:31:33 -0800

    preliminary create van route

--- app/forms/van_image_form.py
+++ app/forms/van_image_form.py
@@ -4,7 +4,7 @@ from wtforms import BooleanField, IntegerField
 from wtforms.validators import DataRequired
 from app.api.aws_helpers import ALLOWED_EXTENSIONS
 
-class VanImage(FlaskForm):
+class VanImageForm(FlaskForm):
   van_id = IntegerField("van id", validators=[DataRequired()])
   image = FileField("image", validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
   preview = BooleanField("preview image", validators=[DataRequired()])

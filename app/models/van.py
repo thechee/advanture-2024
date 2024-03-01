5f12e5d3c49d0b75e5fe0540b6908841c715720b
Chase <chase.agee@gmail.com>
2024-03-01 08:54:45 -0800

    delete is functional

--- app/models/van.py
+++ app/models/van.py
@@ -32,7 +32,7 @@ class Van(db.Model):
   owner = db.relationship("User", back_populates="vans")
   fuel_type = db.relationship("FuelType", back_populates="vans")
   features = db.relationship("Feature", secondary=van_features, back_populates="vans")
-  images = db.relationship("VanImage", back_populates="van")
+  images = db.relationship("VanImage", back_populates="van", cascade='all, delete-orphan')
 
   def to_dict(self):
     owner = self.owner.to_dict()

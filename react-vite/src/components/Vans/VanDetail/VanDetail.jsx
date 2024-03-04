9cbb369071c38f7f229bbe23ed98ec309cfb7ccd
Chase <chase.agee@gmail.com>
2024-03-04 10:35:55 -0800

    added ratings and bars into van details, finished rating list item component

--- react-vite/src/components/Vans/VanDetail/VanDetail.jsx
+++ react-vite/src/components/Vans/VanDetail/VanDetail.jsx
@@ -6,21 +6,24 @@ import { FaRegHeart } from "react-icons/fa";
 import OpenModalButton from "../../OpenModalButton";
 import { DeleteVanModal } from "../DeleteVanModal/DeleteVanModal.jsx";
 import "./VanDetail.css";
+import { RatingsBar } from "../../Ratings/RatingsBar/RatingsBar.jsx";
+import { RatingsListItem } from "../../Ratings/RatingsListItem/RatingsListItem.jsx";
 
 export const VanDetail = () => {
   const navigate = useNavigate()
   const dispatch = useDispatch();
   const { vanId } = useParams();
-  const van = useSelector((state) => state.vans[vanId]);
-  const user = useSelector((state) => state.session.user);
+  const van = useSelector(state => state.vans[vanId]);
+  const user = useSelector(state => state.session.user);
+  const ratingsObj = useSelector(state => state.vans[vanId]?.ratings)
 
   useEffect(() => {
     dispatch(thunkGetOneVan(vanId));
   }, [dispatch, vanId]);
 
-  if (!van) return <h1>404! VAN NOT FOUND</h1>;
+  if (!van) return null;
 
-  function formatDate(dateStr) {
+  function formatShortDate(dateStr) {
     const date = new Date(dateStr);
     const month = date.toLocaleString("en-us", { month: "short" });
     const year = date.getUTCFullYear();
@@ -29,11 +32,40 @@ export const VanDetail = () => {
 
   function favoriteHandler() {}
 
-  const previewImage = van.images.find(
-    (image) => image.preview == true
-  ).imageUrl;
-  const joinedDate = formatDate(van.owner.createdAt);
+  let previewImage;
+  for (const image in van.images) {
+    if (van.images[image].preview == true) {
+      previewImage = van.images[image].imageUrl
+      break;
+    }
+  }
+
+  const joinedDate = formatShortDate(van.owner.createdAt);
   const owner = user?.id == van.owner.id;
+  const ratings = Object.values(ratingsObj)
+
+  const ratingsTotals = {
+    cleanliness: 0,
+    maintenance: 0,
+    communication: 0,
+    convenience: 0,
+    accuracy: 0,
+  }
+
+  for (const rating in ratingsObj) {
+    for (const [key, value] of Object.entries(ratingsObj[rating])) {
+      if (key == "cleanliness") ratingsTotals.cleanliness += value
+      if (key == "maintenance") ratingsTotals.maintenance += value
+      if (key == "communication") ratingsTotals.communication += value
+      if (key == "convenience") ratingsTotals.convenience += value
+      if (key == "accuracy") ratingsTotals.accuracy += value}
+  }
+  const avgCleanliness = ratingsTotals.cleanliness / ratings.length
+  const avgMaintenance = ratingsTotals.maintenance / ratings.length
+  const avgCommunication = ratingsTotals.communication / ratings.length
+  const avgConvenience = ratingsTotals.convenience / ratings.length
+  const avgAccuracy = ratingsTotals.accuracy / ratings.length
+  const ratingsSum = (avgCleanliness + avgMaintenance + avgCommunication + avgConvenience + avgAccuracy) / 5
 
   return (
     <div>
@@ -49,7 +81,7 @@ export const VanDetail = () => {
             {van.make} {van.model} {van.year}
           </h1>
           <div>
-            <span>AVG RATING GOES HERE</span>
+            <span>{ratingsSum.toString().length == 1 ? ratingsSum.toFixed(1) : ratingsSum.toFixed(2)}</span>
           </div>
           <div className="van-detail-details">
             <ul className="details-ul">
@@ -80,7 +112,34 @@ export const VanDetail = () => {
             </ul>
           </div>
           <h4>RATINGS AND REVIEWS</h4>
-          <h4 style={{ color: "#808080" }}>REVIEWS</h4>
+
+          {ratings.length ? 
+            <div>
+              <div>
+                <p>
+                  {ratingsSum.toString().length == 1 ? ratingsSum.toFixed(1) : ratingsSum.toFixed(2)}
+                </p>
+                <p>({ratings.length} ratings)</p>
+              </div>
+              <div>
+                <div className="rating"><span>Cleanliness</span><RatingsBar ratingAvg={avgCleanliness}/><span>{avgCleanliness.toFixed(1)}</span></div>
+                <div className="rating"><span>Maintenance</span><RatingsBar ratingAvg={avgMaintenance}/><span>{avgMaintenance.toFixed(1)}</span></div>
+                <div className="rating"><span>Communication</span><RatingsBar ratingAvg={avgCommunication}/><span>{avgCommunication.toFixed(1)}</span></div>
+                <div className="rating"><span>Convenience</span><RatingsBar ratingAvg={avgConvenience}/><span>{avgConvenience.toFixed(1)}</span></div>
+                <div className="rating"><span>Accuracy</span><RatingsBar ratingAvg={avgAccuracy}/><span>{avgAccuracy.toFixed(1)}</span></div>
+              </div>
+              <div>
+                <h4 style={{ color: "#808080" }}>REVIEWS</h4>
+                <ul>
+                  {ratings.map(rating => (
+                    <RatingsListItem key={rating.id} rating={rating}/>
+                  ))}
+                </ul>
+              </div>
+            </div> :
+            <div>
+              <h3>This van is not yet rated or reviewed!</h3>  
+            </div>}
         </div>
 
         <div className="van-detail-right-div">

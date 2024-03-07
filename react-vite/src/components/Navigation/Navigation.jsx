2b465d68f71e874f3ca91bc43a5e9a724ea7a70e
Chase <chase.agee@gmail.com>
2024-03-07 15:33:52 -0800

    fixed nav to not render all vans buton when at all vans. fixed issue with rating that allowed for an empty rating to be sent.

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,10 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
 import { useSelector } from "react-redux";
 
 function Navigation() {
   const user = useSelector(state => state.session.user)
+  const location = useLocation()
 
   return (
     <header className="nav-bar">
@@ -14,11 +15,11 @@ function Navigation() {
       </Link>
       <div id="empty-header">
       </div>
-      <Link to={"/vans"}>
+      {!location.pathname.endsWith("/vans") && <Link to={"/vans"}>
       <div id="host-div">
-        <span>See all vans</span>
+        <span>View all vans</span>
       </div>
-      </Link>
+      </Link>}
       {user && <Link to='/vans/new'>
       <div id="host-div">
         <span>Add a van</span>
--- react-vite/src/components/Ratings/ManageRatings/UserRating/UserRating.jsx
+++ react-vite/src/components/Ratings/ManageRatings/UserRating/UserRating.jsx
@@ -24,6 +24,7 @@ export const UserRating = ({ rating }) => {
     e.preventDefault();
     e.stopPropagation();
 
+    
   };
 
 

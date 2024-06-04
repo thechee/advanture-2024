93b295425f2cebf1e591ece8cd561d35a27a0b96
Chase Agee <105754333+thechee@users.noreply.github.com>
2024-06-04 16:58:51 -0700

    styling calendar

--- react-vite/src/components/Vans/VanDetail/DateInput.jsx
+++ react-vite/src/components/Vans/VanDetail/DateInput.jsx
@@ -1,21 +1,31 @@
 import { useEffect, useState } from "react";
-import { format, add } from "date-fns";
+import { add } from "date-fns";
 import { thunkCreateVanBooking } from "../../../redux/van";
 import { useDispatch } from "react-redux";
 import { useLocation, useNavigate } from "react-router-dom";
+import DatePicker from "react-datepicker";
+import './DatePickerStyles.css';
 
 export const DateInput = ({van, formRef}) => {
   const dispatch = useDispatch();
   const location = useLocation();
   const navigate = useNavigate();
-  const [start, setStart] = useState(format(new Date(), "yyyy-MM-dd"));
-  const [end, setEnd] = useState(format(add(new Date(), { days: 3 }), "yyyy-MM-dd"));
+  const [start, setStart] = useState(new Date());
+  const [end, setEnd] = useState(add(new Date(), { days: 3 }));
 
   useEffect(() => {
     if (start > end) {
-      setEnd(start);
+      setEnd(add(start, { days: 1 }));
     }
   }, [start, end]);
+
+  const blockedDates = Object.values(van.bookings).map(booking => {
+    return {
+      start: new Date(new Date(booking.startDate).setHours(0, 0, 0, 0)),
+      end: new Date(new Date(booking.endDate).setHours(0, 0, 0, 0))
+    }
+  })
+  console.log(blockedDates)
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -26,21 +36,37 @@ export const DateInput = ({van, formRef}) => {
     }
   }
 
-  console.log("start:", start, "end:", end)
-
   return (
     <form className="van-detail-trip-form" ref={formRef} onSubmit={handleSubmit}>
       <label>Trip Start</label>
-      <input
+      {/* <input
         type="date"
         value={start}
         onChange={(e) => setStart(e.target.value)}
+      /> */}
+      <DatePicker 
+        selected={start}
+        selectsStart
+        onChange={date => setStart(new Date(date))}
+        dateFormat="MM/dd/yyyy"
+        excludeDateIntervals={blockedDates}
+        startDate={start}
+        endDate={end}
       />
       <label>Trip End</label>
-      <input
+      {/* <input
         type="date"
         value={end}
         onChange={(e) => setEnd(e.target.value)}
+      /> */}
+      <DatePicker 
+        selected={end}
+        selectsEnd
+        onChange={date => setEnd(new Date(date))}
+        dateFormat="MM/dd/yyyy"
+        excludeDateIntervals={blockedDates}
+        startDate={start}
+        endDate={end}
       />
       <button
         onClick={handleSubmit}

07be477237e7d2eef1e7d45a1f6810691e8a51b2
Chase Agee <105754333+thechee@users.noreply.github.com>
2024-06-25 23:18:22 -0700

    started trips history

--- react-vite/src/components/Users/Trips/Trips.jsx
+++ react-vite/src/components/Users/Trips/Trips.jsx
@@ -1,10 +1,12 @@
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Trips.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { thunkGetUserBookings } from '../../../redux/session';
 
 export const Trips = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const user = useSelector(state => state.session.user);
 
   useEffect(() => {
@@ -30,7 +32,13 @@ export const Trips = () => {
       <h1>Trips</h1>
       <div>
         <img src="https://resources.turo.com/client/v2/builds/assets/il_car_on_the_desert_@2xc6729191106bba04b948.png" alt="" />
-        <h2>feature coming soon</h2>
+        <h2>No upcoming trips yet</h2>
+        <p>Explore incredible camper vans and book your next trip.</p>
+        <button className='submit-btn' onClick={() => navigate('/')}>Start searching</button>
+      </div>
+      <h3>History</h3>
+      <div>
+        
       </div>
     </div>
   )
